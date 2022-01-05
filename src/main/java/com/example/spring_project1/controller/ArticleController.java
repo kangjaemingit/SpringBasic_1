7eaf802222fa12882c4b367be83c053e8d6784a6
kangjaemingit <71342315+kangjaemingit@users.noreply.github.com>
2022-01-06 01:52:20 +0900

    - 데이터 수정 폼 생성 - 데이터 수정 - 데이터 삭제

--- src/main/java/com/example/spring_project1/controller/ArticleController.java
+++ src/main/java/com/example/spring_project1/controller/ArticleController.java
@@ -7,11 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -68,5 +68,58 @@ public class ArticleController {
         // 뷰페이지 설정
         return "articles/index"; // articles/index.mustache
     }
+
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        // 수정할 데이터를 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 모델에 데이터 등록
+        model.addAttribute("article", articleEntity);
+
+        // 뷰 페이지 설정
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info(form.toString());
+
+        // DTO를 엔티티로 변환
+        Article articleEntity = form.toEntity();
+        log.info(articleEntity.toString());
+
+        // 엔티티를 DB로 저장한다
+        // DB에서 기존 데이터를 가져온다
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        // 기존 데이터에 값을 갱신
+        if(target != null){
+            articleRepository.save(articleEntity);
+        }
+
+        // 수정 결과 페이지로 리다이렉트 한다
+        return "redirect:/articles/" + articleEntity.getId();
+
+    }
+
+    @GetMapping("/articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr){
+        log.info("삭제요청");
+
+        // 삭제 요청을 가져온다
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+
+        // 대상을 삭제한다
+        if(target != null){
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제가 완료 되었습니다!");
+        }
+
+        // 결과페이지로 리다이렉트
+        return "redirect:/articles";
+    }
+
+
 
 }
